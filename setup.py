334129ec738a216a3263a8818a83adbb9487eeff
Orne Brocaar <info@brocaar.com>
2010-05-26 10:21:42 +0200

    setup.py added so this package can be installed with pip.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='django-form-designer',
+    version='0.1',
+    description='A Django app for building many kinds of forms visually, without any programming knowledge.',
+    author='Samuel Luescher',
+    url='http://github.com/philomat',
+    packages=[
+        'form_designer',
+        'form_designer.templatetags',
+    ]
+)
