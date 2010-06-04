eb38c8e0a9877502e5db420bbe1418cdd216d9d8
Jannis Leidel <jannis@leidel.info>
2010-06-04 09:27:36 +0200

    Bumped version up.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = read('README.md')
 
 setup(
     name = "django-form-designer",
-    version = "0.1a1",
+    version = "0.1a2",
     url = 'http://github.com/philomat/django-form-designer',
     license = 'BSD',
     description = "Design contact forms, search forms etc from the Django admin, without writing any code. Integrates with Django CMS.",
